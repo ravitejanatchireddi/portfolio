bb72782302e90aae75b884780f01f622c3ea51e4
Ravi Teja Natchireddi <160053342+ravitejanatchireddi@users.noreply.github.com>
2024-02-18 10:21:42 +0000

    Update siteProps in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ravi Teja Natchireddi",
+  title: "Software Engineer & Content Creator",
+  email: "raviteja@intocse.com",
+  gitHub: "ravitejanatchireddi",
+  instagram: "",
+  linkedIn: "raviteja-n",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "daredevilteja",
+  youTube: "IntoCSE",
 };
 
 const primaryColor = "#4E567E";
